137f4d0e76a30674270a7ffd459e86aa62269091
Allar Vendla <allarvendla@gmail.com>
2016-10-02 21:51:52 +0300

    initial commit

---
+++ src/main/helloWorld.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by Allar on 02.10.2016.
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
